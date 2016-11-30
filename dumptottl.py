4ef0fd0222abcc81d0a6b81572035287e56c6b96
LAPTOP-UVVFKMT6\enzoc <enzo.cerfogli@gmail.com>
2016-11-30 11:56:29 -0300

    parser the oredictionary file

--- dumptottl.py
+++ dumptottl.py
@@ -21,12 +21,28 @@ def main():
         item_dics[id_item]=item_des
         line=arc_itemname.readline()
     #%%
-    str_itemname="vsdump-2015-07-17T143502-items.log"
-    arc_itemname = open(str_itemname)
-    line=arc_itemname.readline()
+    str_itemname="vsdump-2015-07-17T143502-oredict.log"
+    arc_itemdict = open(str_itemname)
+    line=arc_itemdict.readline()
     item_dics=dict()
     while line != '':
+        gr=line.split("!")
+        rel=gr[1].split("->")
+        print(rel[0] + " " + rel[1])
+        ms = rel[1].split(")")
+        print(ms)
+        tl=[]
+        for m in ms:
+            if not m =='\n':
+                n_str=m[1:-2]
+                n_gr=n_str.split(":")
+                temp_str=n_gr[0]+n_gr[1]
+                n_int=int(n_gr[0]+n_gr[1]) 
+                tl.append(n_int)
+        item_dics[rel[0]]=tl
+        line=arc_itemdict.readline()
 #%%
 if __name__ == '__main__':
     main()
 #interfase or antologi, external wiki data, rdflib
+# demo not screen shot
