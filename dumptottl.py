b51b933032fe8cd87878223369f844f23c7469f9
LAPTOP-UVVFKMT6\enzoc <enzo.cerfogli@gmail.com>
2016-11-30 11:24:07 -0300

    finalaer the dict for item

--- dumptottl.py
+++ dumptottl.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
+# -*- coding: latin-1 -*-
 
 import re
-
+#%%
 def main():
-	str_itemname="vsdump-2015-07-17T143502-items.log"
-	arc_itemname = open(str_itemname)
-	line=arc_itemname.readline()
-	item_name=dict()
-	while line != '':
-		# procesar line
-		print (line.split("||")[1] )
-		m = re.search("recipedumper:item!(\d+):(\d+) U=([a-zA-Z0-9._ :|'-/§]*)\|\|L=([a-zA-Z0-9 '§]*)", line)
-		print (str(m.group(1))+" "+str(m.group(2))+" "+str(m.group(3))+" "+str(m.group(4)))
-
-		id_item=int(m.group(1))*pow(10,len(m.group(2)))+int(m.group(2))
-		nombre=m.group(4)
-		item_name[id_item]=nombre
-		print (str(id_item)+" "+str(nombre) + "\n")
-		line=arc_itemname.readline()
-
+    #%%
+    str_itemname="vsdump-2015-07-17T143502-items.log"
+    arc_itemname = open(str_itemname)
+    line=arc_itemname.readline()
+    item_dics=dict()
+    while line != '':
+		# procesar lin
+        m = re.search("recipedumper:item!(\d+):(\d+) (.*)", line)
+        name=line.split("||")[1]
+        name=name[2:]
+        id_item=int(m.group(1))*pow(10,len(m.group(2)))+int(m.group(2))
+        item_des=dict()
+        item_des={"name":name,"id":id_item,"id_1":m.group(1),"id_2":m.group(2),
+                  "name_intern":m.group(3)}
+        item_dics[id_item]=item_des
+        line=arc_itemname.readline()
+    #%%
+    str_itemname="vsdump-2015-07-17T143502-items.log"
+    arc_itemname = open(str_itemname)
+    line=arc_itemname.readline()
+    item_dics=dict()
+    while line != '':
+#%%
 if __name__ == '__main__':
-	print ("hola || chao".split(" || "))
-	main()
+    main()
 #interfase or antologi, external wiki data, rdflib
