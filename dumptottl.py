67e3fd79412591db8a7aa910a027e5c454b45d08
LAPTOP-UVVFKMT6\enzoc <enzo.cerfogli@gmail.com>
2016-12-05 10:43:35 -0300

    added first proceser

--- dumptottl.py
+++ dumptottl.py
@@ -24,23 +24,44 @@ def main():
     str_itemname="vsdump-2015-07-17T143502-oredict.log"
     arc_itemdict = open(str_itemname)
     line=arc_itemdict.readline()
-    item_dics=dict()
+    item_dics_oredictinary=dict()
     while line != '':
         gr=line.split("!")
         rel=gr[1].split("->")
-        print(rel[0] + " " + rel[1])
+#        print(rel[0] + " " + rel[1])
         ms = rel[1].split(")")
-        print(ms)
         tl=[]
         for m in ms:
             if not m =='\n':
-                n_str=m[1:-2]
-                n_gr=n_str.split(":")
+                n_str=m[1:]
+                n_str2=n_str.split(",")
+                n_gr=n_str2[0].split(":")
                 temp_str=n_gr[0]+n_gr[1]
+                print(temp_str)
                 n_int=int(n_gr[0]+n_gr[1]) 
                 tl.append(n_int)
-        item_dics[rel[0]]=tl
+        item_dics_oredictinary[rel[0]]=tl
         line=arc_itemdict.readline()
+        #%%
+	line_recipe =[]
+	with open('vsdump-2015-07-17T143500-recipes.log') as f:
+		lines = f.readlines()
+		for line in lines:
+			dict_recipie = {'type_crafting':'', 'coordinate':[] ,'result':''}
+			coordinates = line.split('!')[1].split('->')[0]
+			for coor in (re.findall(r"\([^\)]*\)",coordinates)):
+				dict_recipie['coordinate'].append(coor)
+			dict_recipie['type_crafting'] = (line.split('!')[0].split(':')[1])
+			dict_recipie['result'] = line.split('!')[1].split('->')[1][:-1]
+			line_recipe.append(dict_recipie)
+#%%
+    to_archive="@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n @prefix : <http://example.com/> .\n"
+    for l in line_recipe:
+        p_n_gr=l["result"].split(",")[0][1:]
+        n_gr=p_n_gr.split(":")
+        n_int=int(n_gr[0]+n_gr[1]) 
+        sujeto=":"+str(n_int)
+        to_archive=to_archive+sujeto+' :amount "'+ l["result"].split(",")[1][:-1]+'"^^xsd:integer . \n'
 #%%
 if __name__ == '__main__':
     main()
