9567a4ffa0dec4dc79383980eb82712660d220bb
LAPTOP-UVVFKMT6\enzoc <enzo.cerfogli@gmail.com>
2016-12-12 11:22:21 -0300

    avances

--- dumptottl.py
+++ dumptottl.py
@@ -17,7 +17,7 @@ def main():
         id_item=int(m.group(1))*pow(10,len(m.group(2)))+int(m.group(2))
         item_des=dict()
         item_des={"name":name,"id":id_item,"id_1":m.group(1),"id_2":m.group(2),
-                  "name_intern":m.group(3)}
+                  "name_intern":m.group(3).split("||")[0][2:]}
         item_dics[id_item]=item_des
         line=arc_itemname.readline()
     #%%
@@ -55,13 +55,47 @@ def main():
 			dict_recipie['result'] = line.split('!')[1].split('->')[1][:-1]
 			line_recipe.append(dict_recipie)
 #%%
-    to_archive="@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n @prefix : <http://example.com/> .\n"
+    tll=open("items.ttl","w")
+    tll.write("@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n@prefix : <http://example.com/> .\n\n")
     for l in line_recipe:
         p_n_gr=l["result"].split(",")[0][1:]
         n_gr=p_n_gr.split(":")
         n_int=int(n_gr[0]+n_gr[1]) 
         sujeto=":"+str(n_int)
-        to_archive=to_archive+sujeto+' :amount "'+ l["result"].split(",")[1][:-1]+'"^^xsd:integer . \n'
+        tll.write(sujeto +' :amount "'+ l["result"].split(",")[1][:-1]+'"^^xsd:integer . \n')
+        tll.write(sujeto +' :typeCrafting '+l["type_crafting"]+' . \n')
+        tll.write(sujeto +' :id "'+l["result"].split(":")[0][1:]+'"^^xsd:integer . \n')
+        tll.write(sujeto +' :subId "'+l["result"].split(":")[1].split(",")[0]+'"^^xsd:integer . \n')
+        tll.write(sujeto +' foaf:name '+item_dics[n_int]['name'][:-1]+' . \n')
+        
+        for c in l["coordinate"]:
+            if "w=" in c:
+                #print(c)
+                tll.write(sujeto +' :w '+c.split(",")[0].split("=")[1] + ". \n")
+                #print(c.split(",")[0].split("=")[1])
+                tll.write(sujeto +' :h '+c.split("=")[2].split(")")[0] + ". \n")
+            elif "@" in c:
+                #print(c)
+                name=c.split(",")[0][1:]
+                tll.write(sujeto +' :craftwith '+ name + " . \n")
+            elif "None" in c:
+                pass
+            else:
+                print(c)
+                c_p_n_gr=c.split(",")[0][1:]
+                c_n_gr=c_p_n_gr.split(":")
+                c_n_int=int(c_n_gr[0]+c_n_gr[1]) 
+                tll.write(sujeto +' :craftwith '+ str(c_n_int) + " . \n")
+                
+        tll.write("\n")
+#%%
+    line_recipe =[]
+    log=open("recipes.log","w")
+    with open('vsdump-2015-07-17T143500-recipes.log') as f:
+        lines = f.readlines()
+        for line in lines:
+            pass
+    
 #%%
 if __name__ == '__main__':
     main()
